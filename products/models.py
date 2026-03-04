b6e097fcdf9c428b3dd5ee403ecd0bea80a698d0
Tupay <61619620+RubyFalcon@users.noreply.github.com>
2026-03-04 16:53:32 +0000

    feat(products): set default=False for featured field

--- products/models.py
+++ products/models.py
@@ -6,7 +6,7 @@ class Product(models.Model):
     description = models.TextField(blank=True, null=True)
     price = models.DecimalField(max_digits=10000, decimal_places=2)
     summary = models.TextField(blank=False, null=False)
-    featured = models.BooleanField()
+    featured = models.BooleanField(default=False)
 
 
     
