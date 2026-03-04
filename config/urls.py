0b0bb30fd0b351ed8719b413c90893981f32559d
Tupay <61619620+RubyFalcon@users.noreply.github.com>
2026-03-04 16:46:45 +0000

    feat(products): add ProductForm and product_create_view

--- config/urls.py
+++ config/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import path
 
 from pages.views import home_view, contact_view,about_view
-from products.views import product_detail_view
+from products.views import product_detail_view, product_create_view
 
 urlpatterns = [
     path("", home_view, name="home"),
@@ -26,5 +26,6 @@ urlpatterns = [
     path("contact/",contact_view,name="contact"),
     path("about/",about_view,name="about"),
     path('admin/', admin.site.urls),
-    path("product/", product_detail_view)
+    path("product/", product_detail_view),
+    path("create", product_create_view)
 ]
--- products/views.py
+++ products/views.py
@@ -1,16 +1,23 @@
 from django.shortcuts import render
 
+from .forms import ProductForm
 from .models import Product
 
 # Create your views here.
+def product_create_view(request):
+    form = ProductForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+
+    context = {
+        "form": form,
+    }
+    
+    return render(request,"products/product_create.html", context)
+
 def product_detail_view(request):
     obj = Product.objects.get(id=1)
-    # Only render title, desc and price for now
-    # context = {
-    #     "title": obj.title,
-    #     "description": obj.description,
-    #     "price": obj.price,
-    # }
+   
     context = {
         'object': obj
     }
