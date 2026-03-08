e71c07374a8ada2794747703cd85e56422df5dc8
Tupay <61619620+RubyFalcon@users.noreply.github.com>
2026-03-08 16:42:17 +0000

    feat(products): add product list view, template, and get_absolute_url method

--- products/views.py
+++ products/views.py
@@ -80,3 +80,9 @@ def product_delete_view(request,id):
     }
     return render(request,"products/product_delete.html",context)
 
+def product_list_view(request):
+    queryset = Product.objects.all()
+    context = {
+        "object_list": queryset
+    }
+    return render(request, "products/product_list.html",context)
