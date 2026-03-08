31e4a1cc2c8fa2caaee1666f6c8bb203646d479b
Tupay <61619620+RubyFalcon@users.noreply.github.com>
2026-03-08 16:56:14 +0000

    refactor(products): rename update view and clean up product views

--- config/urls.py
+++ config/urls.py
@@ -18,7 +18,14 @@ from django.contrib import admin
 from django.urls import path
 
 from pages.views import home_view, contact_view,about_view
-from products.views import product_detail_view, product_create_view, render_initial_data,dynamic_lookup_view, product_delete_view, product_list_view
+
+from products.views import (
+    product_detail_view, 
+    product_create_view, 
+    product_update_view,
+    dynamic_lookup_view, 
+    product_delete_view, 
+    product_list_view)
 
 urlpatterns = [
     path("", home_view, name="home"),
@@ -29,7 +36,7 @@ urlpatterns = [
     path("product/", product_detail_view),
     path("products/<int:id>/",dynamic_lookup_view, name="product-detail"),
     path("create/", product_create_view),
-    path("initial/",render_initial_data),
+    path("initial/",product_update_view),
     path("products/<int:id>/delete/", product_delete_view ,name="product-delete"),
     path("products/",product_list_view, name="product-list")
 ]
