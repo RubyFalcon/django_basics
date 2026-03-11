799b0131dd90831950e21b6e8765b9610b232f52
Tupay <61619620+RubyFalcon@users.noreply.github.com>
2026-03-11 15:42:17 +0000

    feat(courses): create Raw List class based View

--- courses/urls.py
+++ courses/urls.py
@@ -1,14 +1,15 @@
 from django.urls import path
 from .views import (
     CourseDetailView,
+    CourseListView,
     # my_fbv
 )
 
 app_name = "courses"
 
 urlpatterns = [
-    path('',CourseDetailView.as_view(template_name="contact.html"), name="courses-list"),
-    path('<int:id>/',CourseDetailView.as_view(), name="courses-list"),
+    path('',CourseListView.as_view(), name="courses-list"),
+    path('<int:id>/', CourseDetailView.as_view(), name="courses-list"),
     # path('',CourseDetailView.as_view(template_name="contact.html"), name="courses-list"),
     # path('',CourseDetailView.as_view(template_name="contact.html"), name="courses-list"),
     # path('',my_fbv, name="courses-list")
--- courses/views.py
+++ courses/views.py
@@ -3,6 +3,21 @@ from django.views import View
 from .models import Course
 
 # BASE VIEW class = VIEW
+
+class CourseListView(View):
+    template_name = "courses/course_list.html"
+    queryset = Course.objects.all()
+
+    def get_queryset(self):
+        return self.queryset
+    
+    def get(self,request,*args,**kwargs):
+        context = {"object_list": self.get_queryset()}
+        return render(request, self.template_name, context )
+    
+
+class MyListView(CourseListView):
+    queryset = Course.objects.filter(id=1)
 # Create your views here.
 class CourseDetailView(View):
     template_name = "courses/course_detail.html"
