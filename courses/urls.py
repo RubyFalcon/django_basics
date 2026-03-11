09ed2c7de8e82f6fb866d366c75b29e50362a88f
Tupay <61619620+RubyFalcon@users.noreply.github.com>
2026-03-11 15:52:39 +0000

    feat(courses): Create course view model and form added

--- courses/urls.py
+++ courses/urls.py
@@ -2,15 +2,16 @@ from django.urls import path
 from .views import (
     CourseDetailView,
     CourseListView,
+    CourseCreateView
     # my_fbv
 )
 
 app_name = "courses"
 
 urlpatterns = [
+    path('create/',CourseCreateView.as_view(), name="courses-create"),
     path('',CourseListView.as_view(), name="courses-list"),
     path('<int:id>/', CourseDetailView.as_view(), name="courses-list"),
-    # path('',CourseDetailView.as_view(template_name="contact.html"), name="courses-list"),
-    # path('',CourseDetailView.as_view(template_name="contact.html"), name="courses-list"),
+    # path('',CourseDetailView.as_view(), name="courses-list"),
     # path('',my_fbv, name="courses-list")
 ]
--- courses/views.py
+++ courses/views.py
@@ -1,9 +1,26 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import View
+
 from .models import Course
-
+from .forms import CourseModelForm
 # BASE VIEW class = VIEW
+class CourseCreateView(View):
+    template_name = "courses/course_create.html"
 
+    def get(self,request, *args, **kwargs):
+        # GET method
+        form = CourseModelForm()
+        context = {"form": form}
+        return render(request, self.template_name, context)
+    
+    def post(self,request, *args, **kwargs):
+        # POST method
+        form = CourseModelForm(request.POST)
+        if form.is_valid():
+            form.save()
+        context = {"form": form}
+        return render(request, self.template_name, context)
+    
 class CourseListView(View):
     template_name = "courses/course_list.html"
     queryset = Course.objects.all()
